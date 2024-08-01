74c92ab2cdaf466993ec48d9af5f1d139a46b4c1
thuyiya <thusitha.4t@gmail.com>
2024-08-01 23:49:36 +0530

    generate system for types with codegen

--- packages/server/codegen.ts
+++ packages/server/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
     schema: 'src/schema/**/*.graphql', 
     generates: {
-        './src/resolvers-types.ts': {
+        './src/types/index.d.ts': {
             plugins: [
                 'typescript',
                 'typescript-resolvers'
