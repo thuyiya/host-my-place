89f71f74f1d402487fe50c39f566ec2f7cdfff86
thuyiya <thusitha.4t@gmail.com>
2024-08-06 02:11:03 +0530

    separate tests using describe

--- packages/server/src/modules/register/__tests__/register.test.ts
+++ packages/server/src/modules/register/__tests__/register.test.ts
@@ -14,67 +14,94 @@ const REGISTER_MUTATION = gql`
     }
 `;
 
-test("Register User", async () => {
+describe("Register User", () => {
     
-    const client = globalThis.client;
-    //lets register new user
-    const { data } = await client.mutate({
-        mutation: REGISTER_MUTATION,
-        variables: { email, password },
-    });
+    it("lets register new user", async () => {
+        const { data } = await globalThis.client.mutate({
+            mutation: REGISTER_MUTATION,
+            variables: { email, password },
+        });
 
-    expect(data).toEqual({ register: null });
+        expect(data).toEqual({ register: null });
 
-    const users = await User.find({ where: { email } });
-    expect(users).toHaveLength(1);
-    const user = users[0];
-    expect(user.email).toEqual(email);
-
-    //lets see for existing users
-    const { data: data2 } = await client.mutate({
-        mutation: REGISTER_MUTATION,
-        variables: { email, password },
-    });
-
-    expect(data2.register).toHaveLength(1)
-    expect(data2.register[0]).toMatchObject({
-        path: 'email',
-        message: REGISTER_CONSTANT.ALREADY_EXIST
+        const users = await User.find({ where: { email } });
+        expect(users).toHaveLength(1);
+        const user = users[0];
+        expect(user.email).toEqual(email);
     })
 
-    //lets see is it bad email
-    const { data: data3 } = await client.mutate({
-        mutation: REGISTER_MUTATION,
-        variables: { email: "wr", password },
-    });
-    expect(data3).toEqual({
-        register : [
-            {
-                "__typename": "Error",
-                path: 'email',
-                message: REGISTER_CONSTANT.EMAIL_NOT_LONG_ENOUGH
-            },
-            {
-                "__typename": "Error",
-                path: 'email',
-                message: REGISTER_CONSTANT.EMAIL_IS_INVALID
-            }
-        ]
+    it("lets see for existing users", async () => {
+        const { data: data2 } = await globalThis.client.mutate({
+            mutation: REGISTER_MUTATION,
+            variables: { email, password },
+        });
+
+        expect(data2.register).toHaveLength(1)
+        expect(data2.register[0]).toMatchObject({
+            path: 'email',
+            message: REGISTER_CONSTANT.ALREADY_EXIST
+        })
     })
 
-    //lets see is it bad password
-    const { data: data4 } = await client.mutate({
-        mutation: REGISTER_MUTATION,
-        variables: { email: email, password: "12" },
-    });
-    expect(data4).toEqual({
-        register: [
-            {
-                "__typename": "Error",
-                path: 'password',
-                message: REGISTER_CONSTANT.PASSWORD_NOT_LONG_ENOUGH
-            }
-        ]
+    it("lets see is it bad email", async () => {
+        const { data: data3 } = await globalThis.client.mutate({
+            mutation: REGISTER_MUTATION,
+            variables: { email: "wr", password },
+        });
+        expect(data3).toEqual({
+            register: [
+                {
+                    "__typename": "Error",
+                    path: 'email',
+                    message: REGISTER_CONSTANT.EMAIL_NOT_LONG_ENOUGH
+                },
+                {
+                    "__typename": "Error",
+                    path: 'email',
+                    message: REGISTER_CONSTANT.EMAIL_IS_INVALID
+                }
+            ]
+        })
     })
-    
+    it("lets see is it bad password", async () => {
+        const { data: data4 } = await globalThis.client.mutate({
+            mutation: REGISTER_MUTATION,
+            variables: { email: email, password: "12" },
+        });
+        expect(data4).toEqual({
+            register: [
+                {
+                    "__typename": "Error",
+                    path: 'password',
+                    message: REGISTER_CONSTANT.PASSWORD_NOT_LONG_ENOUGH
+                }
+            ]
+        })
+    })
+
+    it("lets see is it bad password and email both", async () => {
+        const { data: data5 } = await globalThis.client.mutate({
+            mutation: REGISTER_MUTATION,
+            variables: { email: "as", password: "12" },
+        });
+        expect(data5).toEqual({
+            register: [
+                {
+                    "__typename": "Error",
+                    path: 'email',
+                    message: REGISTER_CONSTANT.EMAIL_NOT_LONG_ENOUGH
+                },
+                {
+                    "__typename": "Error",
+                    path: 'email',
+                    message: REGISTER_CONSTANT.EMAIL_IS_INVALID
+                },
+                {
+                    "__typename": "Error",
+                    path: 'password',
+                    message: REGISTER_CONSTANT.PASSWORD_NOT_LONG_ENOUGH
+                }
+            ]
+        })
+    })
 });
